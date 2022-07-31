9f7630f5fb20a92f9cb7f3de06528071e4fbc8f2
abemaru <ch4nge.lightening@gmail.com>
2022-07-31 20:36:58 +0900

    goにairを導入して下準備完了

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # my_websocket_demo
 Practice websocket for me
+
---
+++ server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func say_hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Abemaru!")
+}
+
+func main() {
+	http.HandleFunc("/", say_hello)
+	http.ListenAndServe(":8080", nil)
+}
